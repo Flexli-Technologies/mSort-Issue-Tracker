bd33e16fb2a2dbed2172a87fd40fbdf591934362
mayoogh-flexli <148973130+mayoogh-flexli@users.noreply.github.com>
2025-11-13 11:35:45 +0530

    Update config.js

--- docs/config.js
+++ docs/config.js
@@ -1,5 +1,5 @@
 window.MSORT_CONFIG = {
-    REPO_OWNER: "mayoogh-flexli",
+    REPO_OWNER: "Flexli-Technologies",
     REPO_NAME: "mSort-Issue-Tracker",
     AUTO_REFRESH_INTERVAL: 30000, // 30 seconds
     GITHUB_API_BASE: "https://api.github.com"
